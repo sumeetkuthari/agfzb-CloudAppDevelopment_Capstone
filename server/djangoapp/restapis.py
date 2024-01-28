839b481595f9b059b6e2e4484506a239d32c0420
sumeetkuthari <32229745+sumeetkuthari@users.noreply.github.com>
2024-01-28 14:24:06 -0600

    Implemented get_dealer_by_id, add_review, and its link in urls.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -40,7 +40,8 @@ def get_request(url, **kwargs):
 
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
-
+def post_request(url, json_payload,**kwargs):
+    requests.post(url, params=kwargs, json=json_payload)
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
 # def get_dealers_from_cf(url, **kwargs):
@@ -72,6 +73,24 @@ def get_dealers_from_cf(url, **kwargs):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
 
+def get_dealer_by_id_from_cf(url, id):
+    context = {}
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url, dealerId=id)
+    if json_result:
+        dealers = json_result
+        # Get the row list in JSON as dealers
+        for dealer in dealers:
+            dealer_doc = dealer
+            if dealer_doc["id"] == id:
+                dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+                results.append(dealer_obj)
+                break
+    return results[0]
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
 # def get_dealer_reviews_from_cf(url, dealer_id):
